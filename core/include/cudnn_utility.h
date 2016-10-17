3b9df9876693c2da8dd148087d16a8001964d318
Shi Dong <shidong1986@gmail.com>
2016-10-17 16:55:54 -0400

    Some implementation

--- core/include/cudnn_utility.h
+++ core/include/cudnn_utility.h
@@ -20,14 +20,33 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#ifndef CORE_INCLUDE_CUDNN_UTILITY_H_ 
+#ifndef CORE_INCLUDE_CUDNN_UTILITY_H_
 #define CORE_INCLUDE_CUDNN_UTILITY_H_
 
+#include <vector>
 #include "cudnn.h"
 
 namespace dnnmark {
 
+template <typename T>
+class TensorManager {
+ private:
+  map<int, cudnnTensorDescriptor_t> bottom_tensors_;
+  map<int, cudnnTensorDescriptor_t> top_tensors_;
 
+  // For convolution layer only
+  map<int, cudnnConvolutionDescriptor_t> conv_tensors_;
+
+  int number_of_layers_;
+ public:
+  void createTensor(int layer_id) {
+  }
+  void setTensor(int n, int c, int h, int w) {
+  }
+  void setTensor(int n, int c, int h, int w,
+                 int stride_n, int stride_c, int stride_h, int stride_w) {
+  }
+}
 
 } // namespace dnnmark
 
