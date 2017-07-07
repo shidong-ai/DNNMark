26f1a3b225ebbbe05c6050a0c62d6d0fbb4dc0e2
doody1986 <shidong1986@gmail.com>
2017-07-07 11:50:14 -0400

    Add CMake support for MIOpen

--- setup.sh
+++ setup.sh
@@ -1,9 +1,26 @@
 #! /bin/sh
 
+if [ $# -ne 1 ]
+then
+  echo "[Error] The setup script requires one additional parameter specifying whether CUDA or HCC is used"
+  echo "Options: [CUDA, HCC]"
+  exit
+fi
+
+OPTION=$1
+
 BUILD_DIR=build
-CUDNN_PATH=${HOME}/cudnn
 if [ ! -d ${BUILD_DIR} ]; then
   mkdir ${BUILD_DIR}
 fi
 cd ${BUILD_DIR}
-cmake -DCUDNN_ROOT=${CUDNN_PATH} ..
+
+if [ ${OPTION} = "CUDA" ]
+then
+  CUDNN_PATH=${HOME}/cudnn
+  cmake -DCUDNN_ROOT=${CUDNN_PATH} ..
+elif [ ${OPTION} = "HCC" ]
+then
+  MIOPEN_PATH=${HOME}/MIOpen
+  cmake -DMIOPEN_ROOT=${MIOPEN_PATH} ..
+fi
