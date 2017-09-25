0c9e3bc7cd7108ebfdd36fd9c8a1ad4acedf8899
Shi Dong <shidong1986@gmail.com>
2017-09-24 22:21:11 -0400

    Modify the queryKernelDB scripts

--- experiments/queryKernelDB.py
+++ experiments/queryKernelDB.py
@@ -22,6 +22,6 @@ arch = sys.argv[2]
 c.execute("SELECT DISTINCT kernelname FROM "+table_name+" WHERE batchsize = "+batchsize+" and arch = '"+arch+"'")
 kernel_name_list = c.fetchall()
 for kernel in kernel_name_list:
-  c.execute("select invocationorder from "+table_name+" where kernelname = '"+kernel[0]+"' and batchsize = "+batchsize)
+  c.execute("select invocationorder from "+table_name+" where kernelname = '"+kernel[0]+"' and batchsize = "+batchsize+" and arch = '"+arch+"'")
   print kernel[0]+","+str(max(c.fetchall())[0])
 
