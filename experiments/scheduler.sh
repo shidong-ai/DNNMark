8d933704c76c6154bd82d1e70f401f5d197961c7
Shi Dong <shidong1986@gmail.com>
2017-12-19 18:35:10 -0500

    Minor change

--- experiments/scheduler.sh
+++ experiments/scheduler.sh
@@ -20,21 +20,21 @@ PERF_PLOT_SCRIPT=exe_param_plot.sh
 MEM_PLOT_SCRIPT=mem_plot.sh
 COMPUTE_METRICS_PLOT_SCRIPT=compute_metric_plot.sh
 
-#echo "Generating GPU trace in " $ARCH
-#${WORK_DIR}/${GPU_TRACE_PROFILER} ${ARCH}
-#echo "DONE"
-#echo "Generating Metrics in " $ARCH
-#${WORK_DIR}/${METRIC_PROFILER} ${ARCH}
-#echo "DONE"
-#echo "Generating performance related plots for " ${ARCH}
-#${WORK_DIR}/${PERF_PLOT_SCRIPT} ${ARCH}
-#echo "DONE"
-#echo "Generating MEM related plots for " ${ARCH}
-#${WORK_DIR}/${MEM_PLOT_SCRIPT} ${ARCH}
-#echo "DONE"
-#echo "Generating Computing related plots for " ${ARCH}
-#${WORK_DIR}/${COMPUTE_METRICS_PLOT_SCRIPT} ${ARCH}
-#echo "DONE"
+echo "Generating GPU trace in " $ARCH
+${WORK_DIR}/${GPU_TRACE_PROFILER} ${ARCH}
+echo "DONE"
+echo "Generating Metrics in " $ARCH
+${WORK_DIR}/${METRIC_PROFILER} ${ARCH}
+echo "DONE"
+echo "Generating performance related plots for " ${ARCH}
+${WORK_DIR}/${PERF_PLOT_SCRIPT} ${ARCH}
+echo "DONE"
+echo "Generating MEM related plots for " ${ARCH}
+${WORK_DIR}/${MEM_PLOT_SCRIPT} ${ARCH}
+echo "DONE"
+echo "Generating Computing related plots for " ${ARCH}
+${WORK_DIR}/${COMPUTE_METRICS_PLOT_SCRIPT} ${ARCH}
+echo "DONE"
 
 COMPARE_TOOLS_DIR=${WORK_DIR}/comparison_tools
 if [ ! -d "${COMPARE_TOOLS_DIR=}" ]; then
