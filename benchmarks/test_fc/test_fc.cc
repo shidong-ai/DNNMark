0041f31d1a8ba11f01013d799c4a30d7917d31cc
Shi <shidong1986@gmail.com>
2018-05-10 14:31:24 -0400

    Add new benchmarks for FC and BCM_FC

---
+++ benchmarks/test_fc/test_fc.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "common.h"
+#include "dnnmark.h"
+#include "usage.h"
+
+using namespace dnnmark;
+
+int main(int argc, char **argv) {
+  INIT_FLAGS(argc, argv);
+  INIT_LOG(argv);
+  LOG(INFO) << "DNNMark suites: Start...";
+  DNNMark<TestType> dnnmark;
+  dnnmark.ParseGeneralConfig(FLAGS_config);
+  dnnmark.ParseLayerConfig(FLAGS_config);
+  dnnmark.Initialize();
+  // Warm up
+  if (FLAGS_warmup) {
+    for (int i = 0; i < 5; i++) {
+      dnnmark.Forward();
+      dnnmark.Backward();
+    }
+  }
+  dnnmark.Forward();
+  dnnmark.Backward();
+  dnnmark.GetTimer()->SumRecords();
+  dnnmark.TearDown();
+  LOG(INFO) << "Total running time(ms): " << dnnmark.GetTimer()->GetTotalTime();
+  LOG(INFO) << "DNNMark suites: Tear down...";
+  return 0;
+}
